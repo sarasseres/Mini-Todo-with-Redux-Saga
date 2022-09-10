91c3dfee4118993d489db58c3dd1ded062589f52
dsaraswati907 <dsaraswati907@gmail.com>
2022-09-09 22:46:17 -0700

    feature product

--- src/components/Products/ProductsComponents.jsx
+++ src/components/Products/ProductsComponents.jsx
@@ -1,1 +1,20 @@
 export const Content = () => {};
+
+export const Header = (props) => {
+  return <h1>{props.value}</h1>;
+};
+
+export const Card = (props) => {
+  return (
+    <div class="card">
+      <img src={props.image} class="card-img-top" alt="gambar" />
+      <div class="card-body">
+        <h5 class="card-title">{props.title}</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">
+          {props.button}
+        </a>
+      </div>
+    </div>
+  );
+};
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,8 @@
-import { useNavigate } from "react-router-dom";
-import * as Components from "../components/Components";
-import * as ProductsComponents from "../components/Products/ProductsComponents"; // EDIT
+import { useNavigate } from 'react-router-dom';
+import * as Components from '../components/Components';
+import * as ProductsComponents from '../components/Products/ProductsComponents'; // EDIT
 
-import "../components/Products/style.css"; // EDIT
+import '../components/Products/style.css'; // EDIT
 
 const Products = () => {
   const navigate = useNavigate();
@@ -11,9 +11,37 @@ const Products = () => {
     <>
       <Components.Container>
         <h1>Halaman Products</h1>
+        <div className="d-flex align-items-center mt-4">
+          <div className="me-4 cursor-pointer" onClick={() => navigate('/products/Baju Wanita Dewasa Pink')}>
+            Baju Wanita Dewasa Pink
+          </div>
+          <div className="me-4 cursor-pointer" onClick={() => navigate('/products/Celana Pria Dewasa Hitam')}>
+            Celana Pria Dewasa Hitam
+          </div>
+          <div className="me-4 cursor-pointer" onClick={() => navigate('/products/Baju Anak Laki-Laki Biru')}>
+            Baju Anak Laki-Laki Biru
+          </div>
+        </div>
+        <div className="row justify-content-between">
+          <div className="col-3 my-5">
+            <ProductsComponents.Card image={require('../assets/images/favicon.png')} title="Ini Card" button="Ini Card Button" />
+          </div>
+          <div className="col-3 my-5">
+            <ProductsComponents.Card image={require('../assets/images/favicon.png')} title="Ini Card Terisi" />
+          </div>
+          <div className="col-3 my-5">
+            <ProductsComponents.Card image={require('../assets/images/favicon.png')} button="Ini Card Mempunyai Button" />
+          </div>
+          <div className="col-3 my-5">
+            <ProductsComponents.Card image={require('../assets/images/favicon.png')} />
+          </div>
+        </div>
+      </Components.Container>
+      <Components.Container>
+        <ProductsComponents.Header value="Halo Teman" />
       </Components.Container>
     </>
   );
-}
+};
 
 export default Products;
