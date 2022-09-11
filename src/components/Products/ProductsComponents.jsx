adc1b3e9869bb5c926fb3f1a8a819920c2cbf27a
dsaraswati907 <dsaraswati907@gmail.com>
2022-09-10 19:00:30 -0700

    no message

--- src/components/Products/ProductsComponents.jsx
+++ src/components/Products/ProductsComponents.jsx
@@ -12,7 +12,7 @@ export const Card = (props) => {
       <div class="card-body">
         <h4 class="card-title text-center">{props.title}</h4>
         <p class="text-center text-lg">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <button onClick={props.onClick} className="btn btn-dark fw-semibold normal px-md-4 mx- text-align-center px-3 rounded-0" align="center">
+        <button onClick={props.onClick} className="btn btn-dark fw-semibold  px-md-4 mx-auto text-align-right px-3 rounded-0 center-block">
           BUY NOW
           <span className="text-primary2 ms-2">
             <i class="fa-solid fa-arrow-right"></i>
