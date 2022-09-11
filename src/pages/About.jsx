559d893f54741e902500a133ea43302900a7a346
AD HASAN <adsansapu@gmail.com>
2022-09-11 16:52:29 +0700

    foto developer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,11 +2,25 @@ import * as Components from "../components/Components";
 import * as AboutComponents from "../components/About/AboutComponents"; // EDIT
 
 import "../components/About/style.css"; // EDIT
+import Images from '../assets/images/about/irham.jpg'
+import Images1 from '../assets/images/about/adedaudhasan.png'
+
 
 const About = () => {
   return (
     <Components.Container>
-      <h1>Halaman About</h1>
+      <h1>Halaman About ini menampilkan tentang profil develope yang membangun aplikasi toko online</h1>
+       <br />
+      <img src={Images}  alt="cart" width="19" className="foto-profil" /> 
+      <p>Nama : Irham</p>
+      <p>Asal : Bogor</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <img src={Images1}  alt="cart" width="19" className="foto-profil" /> 
+      <p>Nama : Ade Daud Hasan</p>
+      <p>Asal : Majalengka</p>
     </Components.Container>
   );
 }
