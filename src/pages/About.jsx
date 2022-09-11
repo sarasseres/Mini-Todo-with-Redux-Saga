5fd121e332d3a14e8fc72289773b792ff273bf01
AD HASAN <adsansapu@gmail.com>
2022-09-11 17:16:51 +0700

    5developerdiabout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,15 +4,17 @@ import * as AboutComponents from "../components/About/AboutComponents"; // EDIT
 import "../components/About/style.css"; // EDIT
 import Images from '../assets/images/about/irham.jpg'
 import Images1 from '../assets/images/about/adedaudhasan.png'
+import Images2 from '../assets/images/about/fitra.jpg'
+import Images3 from '../assets/images/about/saras.jpg'
 
 
 const About = () => {
   return (
     <Components.Container>
-      <h1>Halaman About ini menampilkan tentang profil develope yang membangun aplikasi toko online</h1>
+      <h1>Halaman About ini menampilkan tentang para profil developer yang membangun aplikasi toko online BHEVIN</h1>
        <br />
       <img src={Images}  alt="cart" width="19" className="foto-profil" /> 
-      <p>Nama : Irham</p>
+      <p>Nama : Irham Muhammad Shidiq</p>
       <p>Asal : Bogor</p>
       <br />
       <br />
@@ -21,6 +23,26 @@ const About = () => {
       <img src={Images1}  alt="cart" width="19" className="foto-profil" /> 
       <p>Nama : Ade Daud Hasan</p>
       <p>Asal : Majalengka</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <img src={Images2}  alt="cart" width="19" className="foto-profil" /> 
+      <p>Nama : Fitra</p>
+      <p>Asal : Bandung</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <img src={Images3}  alt="cart" width="19" className="foto-profil" /> 
+      <p>Nama : Dewi</p>
+      <p>Asal : Bandung</p>
+      <br />
+      <br />
+      <br />
+      <br />
+      <p>Nama : Ade Ilham</p>
+      <p>Asal : Bogor</p>
     </Components.Container>
   );
 }
