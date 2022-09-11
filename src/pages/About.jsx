e157ce53be433b71456fae2968d0b8cdc0565f2d
AD HASAN <adsansapu@gmail.com>
2022-09-11 19:09:18 +0700

    complite about photo

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,6 +6,7 @@ import Images from '../assets/images/about/irham.jpg'
 import Images1 from '../assets/images/about/adedaudhasan.png'
 import Images2 from '../assets/images/about/fitra.jpg'
 import Images3 from '../assets/images/about/saras.jpg'
+import Images4 from '../assets/images/about/adeilham.jpeg'
 
 
 const About = () => {
@@ -13,36 +14,42 @@ const About = () => {
     <Components.Container>
       <h1>Halaman About ini menampilkan tentang para profil developer yang membangun aplikasi toko online BHEVIN</h1>
        <br />
+       
+       
       <img src={Images}  alt="cart" width="19" className="foto-profil" /> 
-      <p>Nama : Irham Muhammad Shidiq</p>
-      <p>Asal : Bogor</p>
+      <p>&nbsp;Nama: Irham Muhammad Shidiq</p>
+      <p>&nbsp;Asal&nbsp;&nbsp;&nbsp;: Bogor</p>
       <br />
       <br />
       <br />
       <br />
       <img src={Images1}  alt="cart" width="19" className="foto-profil" /> 
-      <p>Nama : Ade Daud Hasan</p>
-      <p>Asal : Majalengka</p>
+      <p>&nbsp;Nama : Ade Daud Hasan</p>
+      <p>&nbsp;Asal&nbsp;&nbsp;&nbsp; : Majalengka</p>
       <br />
       <br />
       <br />
       <br />
       <img src={Images2}  alt="cart" width="19" className="foto-profil" /> 
-      <p>Nama : Fitra</p>
-      <p>Asal : Bandung</p>
+      <p>&nbsp;Nama : Fitra Ari Pradhana</p>
+      <p>&nbsp;Asal &nbsp;&nbsp;&nbsp;: Bandung</p>
       <br />
       <br />
       <br />
       <br />
       <img src={Images3}  alt="cart" width="19" className="foto-profil" /> 
-      <p>Nama : Dewi</p>
-      <p>Asal : Bandung</p>
+      <p>&nbsp;Nama : Dewi L</p>
+      <p>&nbsp;Asal &nbsp;&nbsp;&nbsp;: Bekasi</p>
       <br />
       <br />
       <br />
       <br />
-      <p>Nama : Ade Ilham</p>
-      <p>Asal : Bogor</p>
+      <img src={Images4} alt="cart" width="19" className="foto-profil"></img>
+      <p>&nbsp;Nama : Ade Ilham</p>
+      <p>&nbsp;Asal &nbsp;&nbsp;&nbsp;: Bogor</p>
+
+      
+      
     </Components.Container>
   );
 }
