adcb4c8b0f33124f39636b58851c3b2f3e4b102f
dsaraswati907 <dsaraswati907@gmail.com>
2022-09-16 20:28:00 -0700

    modal product revisi

--- src/components/Clothes/ModalProduct.jsx
+++ src/components/Clothes/ModalProduct.jsx
@@ -1,117 +1,113 @@
 import React, { useState, useEffect } from 'react';
 import { Link, Redirect, useHistory, useParams } from 'react-router-dom';
+import './style.css';
 // import { connect, useDispatch, useSelector } from 'react-redux';
 // import { bindActionCreators } from 'redux';
 import { Container, Row, Col, Spinner, Button } from 'reactstrap';
 import { Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Form, FormGroup, Label, Input } from 'reactstrap';
 
 export const ModalProduct = ({ showModal, toggle }) => {
   // let modalOpen = props;
 
-  const updateProfile = (e) => {
+  // const updateProfile = (e) => {
+  //   e.preventDefault();
+  //   const data = {
+  //     name: 'name',
+  //     username: 'username',
+  //     email: 'email',
+  //     gender: 'gender',
+  //     image: 'image',
+  //     password: 'password',
+  //   };
+  // };
+  const [isLoading, setIsLoading] = useState(false);
+  const [input, setInput] = useState({
+    due_date: null,
+    prize: null,
+    title: '',
+    announcement: '',
+    description: '',
+    poster: null,
+  });
+
+  const handleChange = (name, value) => {
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const handlePoster = (e) => {
+    setInput({
+      ...input,
+      poster: e.target.files[0],
+    });
+  };
+
+  const submit = (e) => {
     e.preventDefault();
-    const data = {
-      name: 'name',
-      username: 'username',
-      email: 'email',
-      gender: 'gender',
-      image: 'image',
-      password: 'password',
-    };
+    let formData = new FormData();
+    formData.append('due_date', input.due_date);
+    formData.append('prize', input.prize);
+    formData.append('title', input.title);
+    formData.append('announcement', input.announcement);
+    formData.append('description', input.description);
+    formData.append('poster', input.poster);
   };
 
   return (
     <div>
       <Modal isOpen={showModal} toggle={toggle} className="">
         <ModalBody className="modalBody">
-          <h1 style={{ fontWeight: '600' }}>Profile</h1>
-          <form noValidate onSubmit={updateProfile}>
-            <div className="form-group">
-              <input
-                noValidate
-                type="name"
-                name="name"
-                placeholder="Full Name"
-                // onChange={(e) => setName(e.target.value)}
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <input
-                noValidate
-                type="username"
-                name="username"
-                // value={username}
-                placeholder="Username"
-                // onChange={(e) => setUsername(e.target.value)}
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <input
-                noValidate
-                type="email"
-                name="email"
-                // value={email}
-                placeholder="E-mail"
-                // onChange={(e) => setEmail(e.target.value)}
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <input
-                noValidate
-                type="gender"
-                name="gender"
-                placeholder="Gender"
-                // value={gender}
-                // onChange={(e) => setGender(e.target.value)}
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <input
-                noValidate
-                type="password"
-                name="password"
-                // value={password}
-                placeholder="Password"
-                // onChange={(e) => setPassword(e.target.value)}
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="image">Profile Picture</label>
-              <input
-                noValidate
-                type="file"
-                name="image"
-                // value={image}
-                placeholder="Profile Picture"
-                className="form-control"
-              />
-            </div>
-            <button type="submit" className="btn mr-2">
-              Update
-            </button>
-          </form>
+          <Container className="formTemplate" style={{ minHeight: '750px' }}>
+            <Row>
+              <Col sm="12" md={{ size: 8, offset: 2 }}>
+                <h1>Product</h1>
+                <Form>
+                  <div className="inputContainer">
+                    <div>
+                      <label for="title">Title</label>
+                      <input type="text" id="title" name="title" placeholder="Product Title" onChange={(e) => handleChange('title', e.target.value)} />
+                    </div>
+                    <div>
+                      <label for="prize">Prize</label>
+                      <input type="number" id="prize" name="prize" placeholder="5000000" onChange={(e) => handleChange('prize', e.target.value)} />
+                    </div>
+                    <div>
+                      {' '}
+                      <label for="date">Due Date</label>
+                      <input type="date" id="date" name="due_date" placeholder="YYYY-MM-DD" onChange={(e) => handleChange('due_date', e.target.value)} />
+                    </div>
+                    <div>
+                      <label for="announcement">Announcement</label>
+                      <input type="date" id="announcement" name="announcement" placeholder="YYYY-MM-DD" onChange={(e) => handleChange('announcement', e.target.value)} />
+                    </div>
+                    <div className="photo">
+                      <label for="poster">Poster</label>
+                      <input type="file" id="poster" name="poster" placeholder="poster" onChange={handlePoster} />
+                    </div>
+                    <div className="description">
+                      <label for="desc">Description</label>
+                      <textarea id="desc" name="desc" onChange={(e) => handleChange('description', e.target.value)} />
+                    </div>
+                  </div>
+                  <Button
+                    color="success"
+                    className="button createButton mx-auto"
+                    onClick={submit}
+                    disabled={isLoading ? 'true' : ''}
+                    style={{ cursor: `${isLoading ? 'not-allowed' : 'pointer'}`, backgroundColor: `${isLoading ? '#A8CC74' : '#8FBD4B'}` }}
+                    title="Create Contest"
+                  >
+                    {isLoading ? <Spinner size="sm" color="light" className="spinner" /> : 'Create'}
+                  </Button>
+                </Form>
+              </Col>
+            </Row>
+          </Container>
         </ModalBody>
       </Modal>
-      {/* <Modal isOpen={modalOpen} fade={false} toggle={toggle}>
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-        <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={toggle}>
-            Do Something
-          </Button>{' '}
-          <Button color="secondary" onClick={toggle}>
-            Cancel
-          </Button>
-        </ModalFooter>
-      </Modal> */}
     </div>
   );
 };
