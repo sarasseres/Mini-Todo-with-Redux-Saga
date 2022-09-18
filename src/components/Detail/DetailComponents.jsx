1b509c2bd8fb4591108bc6c27e47cc831347ce45
fitraari <fitraari3114@gmail.com>
2022-09-18 16:37:16 +0700

    Update Detail Product

--- src/components/Detail/DetailComponents.jsx
+++ src/components/Detail/DetailComponents.jsx
@@ -1,74 +1,6 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
-// export const DetailProductCarousel = () => {
-//   return (
-//     <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
-//       <div className="carousel-indicators">
-//         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-//         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-//         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-//         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-//       </div>
-//       <div className="carousel-inner">
-//         <div className="carousel-item active">
-//           <img src={require("../../assets/images/khaki-cord-pocket-front-overshirt.jpg")} className="d-block w-100" alt="Khaki Cord Shirt" />
-//         </div>
-//         <div className="carousel-item">
-//           <img src={require("../../assets/images/khaki-cord-pocket-front-overshirt-2.jpg")} className="d-block w-100" alt="Khaki Cord Shirt 2" />
-//         </div>
-//         <div className="carousel-item">
-//           <img src={require("../../assets/images/khaki-cord-pocket-front-overshirt-3.jpg")} className="d-block w-100" alt="Khaki Cord Shirt 3" />
-//         </div>
-//         <div className="carousel-item">
-//           <img src={require("../../assets/images/khaki-cord-pocket-front-overshirt-4.jpg")} className="d-block w-100" alt="Khaki Cord Shirt 4" />
-//         </div>
-//       </div>
-//       <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-//         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-//         <span className="visually-hidden">Previous</span>
-//       </button>
-//       <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-//         <span className="carousel-control-next-icon" aria-hidden="true"></span>
-//         <span className="visually-hidden">Next</span>
-//       </button>
-//     </div>
-//   );
-// }
-
-// export const DetailProductCarousel = () => {
-//   return (
-//     <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="true">
-//       <div className="carousel-indicators">
-//         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-//         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-//         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-//         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
-//       </div>
-//       <div className="carousel-inner">
-//         <div className="carousel-item active">
-//           <img src={require("../../assets/images/khaki-cord-pocket-front-overshirt.jpg")} className="d-block w-100" alt="Khaki Cord Shirt" />
-//         </div>
-//         <div className="carousel-item">
-//           <img src={require("../../assets/images/khaki-cord-pocket-front-overshirt-2.jpg")} className="d-block w-100" alt="Khaki Cord Shirt 2" />
-//         </div>
-//         <div className="carousel-item">
-//           <img src={require("../../assets/images/khaki-cord-pocket-front-overshirt-3.jpg")} className="d-block w-100" alt="Khaki Cord Shirt 3" />
-//         </div>
-//         <div className="carousel-item">
-//           <img src={require("../../assets/images/khaki-cord-pocket-front-overshirt-4.jpg")} className="d-block w-100" alt="Khaki Cord Shirt 4" />
-//         </div>
-//       </div>
-//       <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-//         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-//         <span className="visually-hidden">Previous</span>
-//       </button>
-//       <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-//         <span className="carousel-control-next-icon" aria-hidden="true"></span>
-//         <span className="visually-hidden">Next</span>
-//       </button>
-//     </div>
-//   );
-// }
 
 function slugToTitle(slug) {
   let prevTitle = slug.split("-");
@@ -85,6 +17,10 @@ export const DetailProduct = () => {
   const params = useParams();
   const title = params.slug === "cloth" ? "Khaki Cord Shirt" : slugToTitle(params.slug);
 
+  const [img, setImg] = useState({
+    src: "khaki-cord-pocket-overshirt",
+  });
+
   return (
     <div className="detail mt-5">
       <div className="row m-0 justify-content-between align-items-center">
@@ -92,24 +28,68 @@ export const DetailProduct = () => {
           <div className="detail-img">
             <div className="detail-img-header">
               <img
-                src={require(`../../assets/images/products/${title === "Khaki Cord Shirt" ? "khaki-cord-pocket-overshirt" : params.slug }.jpg`)}
+                src={require(`../../assets/images/products/${title === "Khaki Cord Shirt" ? img.src : params.slug}.jpg`)}
                 alt="Khaki Cord Shirt"
               />
             </div>
 
             <div className="detail-img-small d-flex">
-              <div className="img" style={{
-                backgroundImage: `url(${require(`../../assets/images/products/${title === "Khaki Cord Shirt" ? "khaki-cord-pocket-overshirt" : params.slug }.jpg`)})`
+              <button onClick={() => {
+                setImg(() => {
+                  return { src: 'khaki-cord-pocket-overshirt', }
+                });
+              }}>
+                <div className="img" style={{
+                  backgroundImage: `url(${require(`../../assets/images/products/${title === "Khaki Cord Shirt" ? "khaki-cord-pocket-overshirt" : params.slug}.jpg`)})`,
+                  border: `${img.border}`
+                }} />
+              </button>
+
+              <button onClick={() => {
+                setImg(() => {
+                  return { src: 'khaki-cord-pocket-overshirt-2', }
+                });
+              }}>
+                <div className="img" style={{
+                  backgroundImage: `url(${require(`../../assets/images/products/${title === "Khaki Cord Shirt" ? "khaki-cord-pocket-overshirt-2" : params.slug}.jpg`)})`,
+                  border: `${img.border}`
+                }} />
+              </button>
+
+              <button onClick={() => {
+                setImg(() => {
+                  return { src: 'khaki-cord-pocket-overshirt-3', }
+                });
+              }}>
+                <div className="img" style={{
+                  backgroundImage: `url(${require(`../../assets/images/products/${title === "Khaki Cord Shirt" ? "khaki-cord-pocket-overshirt-3" : params.slug}.jpg`)})`,
+                  border: `${img.border}`
+                }} />
+              </button>
+
+              <button onClick={() => {
+                setImg(() => {
+                  return { src: 'khaki-cord-pocket-overshirt-4', }
+                });
+              }}>
+                <div className="img" style={{
+                  backgroundImage: `url(${require(`../../assets/images/products/${title === "Khaki Cord Shirt" ? "khaki-cord-pocket-overshirt-4" : params.slug}.jpg`)})`,
+                  border: `${img.border}`
+                }} />
+              </button>
+
+              {/* <div className="img" style={{
+                backgroundImage: `url(${require(`../../assets/images/products/${title === "Khaki Cord Shirt" ? "khaki-cord-pocket-overshirt" : params.slug}.jpg`)})`
               }} />
               <div className="img" style={{
-                backgroundImage: `url(${require(`../../assets/images/products/${title === "Khaki Cord Shirt" ? "khaki-cord-pocket-overshirt-2" : params.slug }.jpg`)})`
+                backgroundImage: `url(${require(`../../assets/images/products/${title === "Khaki Cord Shirt" ? "khaki-cord-pocket-overshirt-2" : params.slug}.jpg`)})`
               }} />
               <div className="img" style={{
-                backgroundImage: `url(${require(`../../assets/images/products/${title === "Khaki Cord Shirt" ? "khaki-cord-pocket-overshirt-3" : params.slug }.jpg`)})`
+                backgroundImage: `url(${require(`../../assets/images/products/${title === "Khaki Cord Shirt" ? "khaki-cord-pocket-overshirt-3" : params.slug}.jpg`)})`
               }} />
               <div className="img" style={{
-                backgroundImage: `url(${require(`../../assets/images/products/${title === "Khaki Cord Shirt" ? "khaki-cord-pocket-overshirt-4" : params.slug }.jpg`)})`
-              }} />
+                backgroundImage: `url(${require(`../../assets/images/products/${title === "Khaki Cord Shirt" ? "khaki-cord-pocket-overshirt-4" : params.slug}.jpg`)})`
+              }} /> */}
             </div>
           </div>
         </div>
