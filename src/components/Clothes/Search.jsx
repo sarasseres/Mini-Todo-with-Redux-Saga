acf64e69d89020d04e347ca7e5d2ca8f3d1752d2
fitraari <fitraari3114@gmail.com>
2022-09-30 13:33:08 +0700

    update feature search

--- src/components/Clothes/Search.jsx
+++ src/components/Clothes/Search.jsx
@@ -4,14 +4,14 @@ const Search = (props) => {
       <div className="input-group mt-5 w-50 mx-auto">
         <input
           type="text"
-          className="form-control"
+          className="form-control search-input"
           placeholder="Search Product"
           aria-label="Recipient's username"
           aria-describedby="button-addon2"
           onChange={(e) => props.onChange(e.target.value)}
         />
         <button
-          className="btn btn-outline-secondary"
+          className="btn btn-outline-secondary search-btn"
           type="button"
           id="button-addon2"
         >
